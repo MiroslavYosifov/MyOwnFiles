f4c4a44ca3c347db90e81a60e3d5cac95df3ffb9
unknown <m.yosifov193@gmail.com>
2021-11-19 00:15:27 +0200

    add slider code

--- SliderAnimation/backup.js
+++ SliderAnimation/backup.js
@@ -4,7 +4,11 @@ var slider = document.getElementById('slider'),
     next = document.getElementById('next');
 
 function slide(wrapper, items, prev, next) {
-  var posInitial,
+  var posX1 = 0,
+      posX2 = 0,
+      posInitial,
+      posFinal,
+      threshold = 100,
       slides = items.getElementsByClassName('slide'),
       slidesLength = slides.length,
       slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
@@ -20,12 +24,62 @@ function slide(wrapper, items, prev, next) {
   items.insertBefore(cloneLast, firstSlide);
   wrapper.classList.add('loaded');
   
+  // Mouse events
+  items.onmousedown = dragStart;
+  
+  // Touch events
+  items.addEventListener('touchstart', dragStart);
+  items.addEventListener('touchend', dragEnd);
+  items.addEventListener('touchmove', dragAction);
+  
   // Click events
   prev.addEventListener('click', function () { shiftSlide(-1) });
   next.addEventListener('click', function () { shiftSlide(1) });
   
   // Transition events
   items.addEventListener('transitionend', checkIndex);
+  
+  function dragStart (e) {
+    e = e || window.event;
+    e.preventDefault();
+    posInitial = items.offsetLeft;
+    
+    if (e.type == 'touchstart') {
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX1 = e.clientX;
+      document.onmouseup = dragEnd;
+      document.onmousemove = dragAction;
+    }
+  }
+
+  function dragAction (e) {
+    e = e || window.event;
+    
+    if (e.type == 'touchmove') {
+      posX2 = posX1 - e.touches[0].clientX;
+      posX1 = e.touches[0].clientX;
+    } else {
+      posX2 = posX1 - e.clientX;
+      posX1 = e.clientX;
+    }
+    items.style.left = (items.offsetLeft - posX2) + "px";
+  }
+  
+  function dragEnd (e) {
+    console.log(posFinal);
+    posFinal = items.offsetLeft;
+    if (posFinal - posInitial < -threshold) {
+      shiftSlide(1, 'drag');
+    } else if (posFinal - posInitial > threshold) {
+      shiftSlide(-1, 'drag');
+    } else {
+      items.style.left = (posInitial) + "px";
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
   
   function shiftSlide(dir, action) {
     items.classList.add('shifting');
